c41b853e673a56aa52c3234639c1e587daacbf4b
Márcia Lima <870062120@edu.taitotalo.fi>
2026-09-21 10:37:30 +0300

    Shop under development

--- shop.py
+++ shop.py
@@ -1,1 +1,11 @@
-print("You're in the shop.")
+def open_shop():
+    print("Welcome to the shop!")
+    buy = input("What would you like to buy?\n A.apple\nB.Gun\nC.Jerky")
+    if buy == A:
+        print('"An apple a day keeps the doctor away."')
+    if buy == B:
+        print("Be careful of the bear.")
+    if buy == C:
+        print("Now you have a snack for later.")
+    else:
+        print("See you again!")    
