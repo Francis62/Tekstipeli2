1966e054dad18968e9b1a6396feb2156a599fc30
Márcia Lima <870062120@edu.taitotalo.fi>
2026-09-21 12:34:32 +0300

    Small text fixes v2

--- shop.py
+++ shop.py
@@ -1,11 +1,11 @@
 def open_shop():
     print("Welcome to the shop!")
     buy = input("What would you like to buy?\nA.apple\nB.Gun\nC.Jerky\nCostumer: ").lower()
-    if buy == "A":
+    if buy == "a":
         print('"An apple a day keeps the doctor away."')
-    if buy == "B":
+    if buy == "b":
         print("Be careful of the bear.")
-    if buy == "C":
+    if buy == "c":
         print("Now you have a snack for later.")
     else:
         print("See you again!")    
