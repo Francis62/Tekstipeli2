63fec9510be03d7aa8425876459531f1220faed3
yuliiiiash <tkachenko2000julya@gmail.com>
2026-09-21 11:14:33 +0300

    Update forest.py
    I changed some details!

--- forest.py
+++ forest.py
@@ -2,7 +2,7 @@ def enter_forest():
     print("Forest is under development")
     print("A bear appears front of you")
 
-    choice= input("Do you want to fffffight? (yes/no):")
+    choice= input("Do you want to fight? (yes/no):")
 
     if (choice=="yes"):
         print("You fight the bear and your ass is kicked and bear will eat you alive ")
