de606ecc63eb1eeac04014cf2e5384889736eab2
yuliiiiash <tkachenko2000julya@gmail.com>
2026-09-21 11:11:18 +0300

    Create forest.py
    Added forest.py.

---
+++ forest.py
@@ -0,0 +1,10 @@
+def enter_forest():
+    print("Forest is under development")
+    print("A bear appears front of you")
+
+    choice= input("Do you want to fffffight? (yes/no):")
+
+    if (choice=="yes"):
+        print("You fight the bear and your ass is kicked and bear will eat you alive ")
+    else:
+        print("you escape back to village")
